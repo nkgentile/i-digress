2e13599203d2a28a207f712503520e7ccd2f75d6
Noah <noah.gentile@gmail.com>
2018-10-09 14:29:29 -0400

    Initial scaffolding of mobile website

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view
+      class="app-content-container"
+    />
   </div>
 </template>
 
 <style lang="less">
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+  }
+
+  #app {
+    display: grid;
+    grid-template-rows: auto 1fr;
+
+    height: 100vh;
+    padding: 0.33rem;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,6 @@
 <template>
+  <main>
+  </main>
 </template>
 
 <script>
